40bb1358c18fec31a54a373c6605ad54920925f6
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2019-12-02 17:10:49 +0530

    added DynamoDB as datastore

--- api.js
+++ api.js
@@ -6,31 +6,15 @@ const getPizzas=require('./handlers/get-pizzas')
 const createOrder=require('./handlers/create-order')
 const updateOrder=require('./handlers/update-order')
 const deleteOrder=require('./handlers/delete-order')
+const getOrders=require('./handlers/get-orders')
 
 api.get('/', () => 'Welcome to Pizza API')
-api.get('/pizzas',()=>{
-    return getPizzas()
-})
-api.get('/pizzas/{id}',(request)=>{
-    return getPizzas(request.pathParams.id)
-},{
-    error: 404
-})
+api.get('/pizzas',()=>{ return getPizzas() })
+api.get('/pizzas/{id}',(request)=>{ return getPizzas(request.pathParams.id) },{ error: 404 })
+api.get('/orders',(request)=>{ return getOrders() },{ error:400 })
+api.get('/orders/{id}',(request)=>{ return getOrders(request.pathParams.id) },{ error:400 })
+api.post('/orders',(request)=>{ return createOrder(request.body) },{ success: 201,error: 400 })
+api.put('/orders/{id}',(request) => { return updateOrder(request.pathParams.id,request.body) },{ error: 400 })
+api.delete('/orders/{id}',(request) => { return deleteOrder(request.pathParams.id) },{ error: 400 })
 
-api.post('/orders',(request)=>{
-    return createOrder(request.body)
-},{
-    success: 201,
-    error: 400
-})
-api.put('/orders/{id}',(request) => {
-    return updateOrder(request.pathParams.id,request.body)
-},{
-    error: 400
-})
-api.delete('/orders/{id}',(request) => {
-    return deleteOrder(request.pathParams.id)
-},{
-    error: 400
-})
 module.exports=api
--- handlers/delete-order.js
+++ handlers/delete-order.js
@@ -1,8 +1,21 @@
-function deleteOrder(id) {
+const AWS=require('aws-sdk')
+const docClient=new AWS.DynamoDB.DocumentClient()
+
+function deleteOrder(orderId) {
     if (!id) {
         throw new Error('To cancel pizza order,please specify order id')
     }
-    return {}
+    return docClient.delete({
+        TableName:'pizza-orders',
+        Key:{
+            orderId:orderId
+        }
+    }).promise()
+    .then(result => console.log('Order is deleted!',result))
+    .catch((deleteError) => {
+        console.log('Oops,order is not deleted :( ',deleteError)
+        throw deleteError
+    })
 }
 
 module.exports=deleteOrder
