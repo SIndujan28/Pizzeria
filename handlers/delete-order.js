55fc671dd7876ceb3b2f4446ee4107b516d8463e
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2019-12-04 23:32:50 +0530

    make only pending order to update/delete

--- handlers/delete-order.js
+++ handlers/delete-order.js
@@ -5,13 +5,30 @@ function deleteOrder(orderId) {
     if (!orderId) {
         throw new Error('To cancel pizza order,please specify order id')
     }
-    return docClient.delete({
+    return docClient.get({
         TableName:'pizza-orders',
         Key:{
             orderId:orderId
         }
     }).promise()
-    .then(result => console.log('Order is deleted!',result))
+    .then(result => result.Item)
+    .then(item => {
+        if( item.orderStatus !== "pending") {
+            throw new Error("Order has started processing,cannot delete order now")
+        }
+    })
+    .then(() => {
+        return docClient.delete({
+            TableName: "pizza-orders",
+            Key:{
+                orderId:orderId
+            }
+        }).promise()
+    })
+    .then((result) => {
+        console.log("Order is deleted",result)
+        return result
+    })
     .catch((deleteError) => {
         console.log('Oops,order is not deleted :( ',deleteError)
         throw deleteError
--- handlers/update-order.js
+++ handlers/update-order.js
@@ -5,22 +5,37 @@ function updateOrder(orderId,options) {
     if( !orderId || !options.pizza || !options.address) {
         throw new Error('Order Id and update object must be specified to update the order')
     }
-    return docClient.update({
+    return docClient.get({
         TableName:'pizza-orders',
         Key:{
             orderId:orderId
-        },
-        UpdateExpression: 'set pizza = :p, address = :a',
-        ExpressionAttributeValues:{
-            ':p':options.pizza,
-            ':a':options.address
-        },
-        ReturnValues:"ALL_NEW"
+        }
     }).promise()
-    .then((result) => {
-        console.log('Order is updated!', result)
-        return result.Attributes
-    }).catch((updateError) => {
+    .then((result) => result.Item)
+    .then(item => {
+        if (item.orderStatus !== "pending") {
+            throw new Error("order has started processing, cannot update now :(")
+        }
+    })
+    .then(() => {
+        return docClient.update({
+            TableName:'pizza-orders',
+            Key:{
+                orderId:orderId
+            },
+            UpdateExpression: 'set pizza = :p, address = :a',
+            ExpressionAttributeValues:{
+                ':p':options.pizza,
+                ':a':options.address
+            },
+            ReturnValues:"ALL_NEW"
+        }).promise()
+        .then((result) => {
+            console.log('Order is updated!', result)
+            return result.Attributes
+        })
+    })
+    .catch((updateError) => {
         console.log(`Oops, order is not updated :(`, updateError)
         throw updateError
     })
